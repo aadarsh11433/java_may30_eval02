b527e7558f0501464f59c4fa933de03611fd6e94
Aadarsh Mukati <aadarsh11433@gmail.com>
2022-05-30 15:50:34 +0530

    eval2

--- Problem1/Animal.java
+++ Problem1/Animal.java
@@ -4,9 +4,17 @@ public class Animal {
 
 	 int legs= 4;
 	 
+	 Diseases disease;
+	 
 	 void habitat() {
 		 System.out.println("lives in jungle");
 	 }
+}
+class Diseases{
+	String viral;
+	String fungal;
+	String Elergies;
+	String Urinary;
 }
 class Cow extends Animal{
 	
@@ -27,3 +35,32 @@ class Cow extends Animal{
 			System.out.println(cow1.legs);
 		}
  }
+ 
+ /*
+           < --------------------------   Is-A relationship in java classes       ---------------------  >
+ 
+           1. Is a relationship in java is established by extending some class to other class 
+             -- like classs Cow is being Extended here to Animal (line 11).
+             
+             -- the class that has extended to some other class is called child class or sub-class.
+             and the class to which aclss is exended is  the Parent or Super Class.
+             
+             Animal -Parent class
+             Cow-    - Child class.
+             
+             even though the cow class doesnt have any data variable named  legs but it has accessed the variable from
+              its parent class.
+              
+               Is A relation can be sum up in a way like Cow is an Animal.
+ 
+ 
+  < --------------------------   Has-A relationship in java classes       ---------------------  >
+ 
+ 
+      Has a relation 
+ 
+ 
+ 
+ 
+ 
+ */
